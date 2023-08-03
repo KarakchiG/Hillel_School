469f03f0d3f74a7debb4007b90c17f9dc4e514a1
kharakchi <karakchi.g@gmail.com>
2023-08-03 21:45:55 +0300

    Added homework_5 and training_imports_homework_5 files

---
+++ training_imports_homework_5.py
@@ -0,0 +1,7 @@
+from homework_5 import processing_arguments, processing_args
+
+result4 = processing_arguments(2, 3)
+print(f"The result is {result4}")
+
+result5 = processing_args(2, 3)
+print(f"The result is {result5}")
